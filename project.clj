d81462752d7966c1f635887c85948fa45a4ca0da
Theo Pack <tf.pack@gmail.com>
2012-11-06 20:34:23 +0100

    add hello world server

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ring/ring-jetty-adapter "1.1.0"]]
+  :plugins [[lein-swank "1.4.4"]]
+  :min-lein-version "2.0.0")
--- src/clojure_kata/core.clj
+++ src/clojure_kata/core.clj
@@ -1,4 +1,13 @@
-(ns clojure-kata.core)
+(ns clojure-kata.core
+  (:use [ring.adapter.jetty :only [run-jetty]]))
+
+(defn app [req]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello, world!"})
+
+(defn -main [port]
+  (run-jetty app {:port (Integer. port)}))
 
 (defn foo
   "I don't do a whole lot."
