cfe74288ce961cc2c48570dd43cbb6641f966915
Theo Pack <tf.pack@gmail.com>
2012-11-06 20:03:11 +0100

    init project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-kata
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clojure_kata/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-kata.core-test
+  (:use clojure.test
+        clojure-kata.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
